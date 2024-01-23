d1519bb7b2b1d5862438469c187e2cfd12cc0f8a
Thinh Nguyen <ngthinh302@gmail.com>
2024-01-23 15:17:09 -0500

    docs(backend-scripts): clearer function annotation and comments for `readCSV`

--- apps/backend/scripts/csvReader.ts
+++ apps/backend/scripts/csvReader.ts
@@ -1,36 +1,34 @@
-import { readFileSync } from 'fs';
-
+import { readFileSync } from "fs";
 
 /**
- * Reads data from a CSV file and returns it as an array of objects.
+ * Parses data from a CSV file. Expects a CSV file with a header row, where each
+ * column header is unique, and has LF line endings.
  *
  * @param {string} filePath - The file system path to the CSV file to be read.
- *
- * @returns {any[]} An array of objects, where each object represents a row from the CSV file,
- *                  with keys corresponding to the column headers and values corresponding to the
- *                  data in each column.
- *
+ * @returns {Record<string, unknown>[]} An array of objects, where each object
+ *                                      represents a row in the CSV file. Each
+ *                                      object has keys corresponding to the
+ *                                      column headers in the CSV file.
  **/
+export const readCSV = (filePath: string): Record<string, unknown>[] => {
+  // Read the file
+  const fileContent = readFileSync(filePath, "utf8");
 
+  // Split the file content by new line to get the rows
+  const lines = fileContent.split("\n");
 
-export function readCSV(filePath: string): any[] {
-    //Read the file
-    const fileContent = readFileSync(filePath, 'utf8');
+  // Extract headers
+  const headers = lines[0].split(",");
 
-    //Split the file content by new line to get the rows
-    const lines = fileContent.split('\n');
-
-    //Extract headers
-    const headers = lines[0].split(',');
-
-    //Parse each line
-    const data = lines.slice(1).map(line => {
-        const values = line.split(',');
-        return headers.reduce((obj, header, index) => {
-            obj[header] = values[index];
-            return obj;
-        }, {} as any);
-    });
-
-    return data;
-}
+  // Parse each line
+  return lines.slice(1).map((line) => {
+    const values = line.split(",");
+    return headers.reduce(
+      (obj, header, index) => {
+        obj[header] = values[index];
+        return obj;
+      },
+      {} as Record<string, unknown>,
+    );
+  });
+};
