91381166e34daf13e5aa9f387e477f26cb77451f
Réda <wayzfut@gmail.com>
2024-01-24 22:41:20 -0500

    changed file names and removed cache

--- apps/frontend/src/routes/MapRoute.tsx
+++ apps/frontend/src/routes/MapRoute.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import imgUrl from "./00_thelowerlevel1.png";
-import "./maproute.css";
+import imgUrl from "./MapImage.png";
+import "./MapRoute.css";
 
 export default function MapRoute() {
   return <img className="level1img" src={imgUrl} alt="Map 1st Floor" />;
