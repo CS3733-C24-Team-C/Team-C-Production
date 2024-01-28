35965f1d4238e0a51237b5597c1e5501f31dbe56
felixng <ngthinh302@gmail.com>
2024-01-28 16:31:42 -0500

    refactor(database): move scripts to database near where they should be run, replace TS types with JSDoc to be interoperable with the plain `node` compiler

--- packages/database/prisma/seed.mjs
+++ packages/database/prisma/seed.mjs
@@ -1,5 +1,5 @@
 import path from "path";
-import { PrismaClient } from '../.prisma/client';
+import { PrismaClient } from "database";
 import { readCSV } from "./csvReader.mjs";
 
 const prisma = new PrismaClient();
@@ -7,33 +7,33 @@ const nodesPath = path.join(path.resolve(), "prisma/L1Nodes.csv");
 const edgesPath = path.join(path.resolve(), "prisma/L1Edges.csv");
 
 const main = async () => {
-    const nodes = readCSV(nodesPath);
-    const edges = readCSV(edgesPath);
+  const nodes = readCSV(nodesPath);
+  const edges = readCSV(edgesPath);
 
-    for (const node of nodes) {
-        await prisma.nodes.create({
-        data: {
-            ...node,
-            xcoord: Number(node.xcoord),
-            ycoord: Number(node.ycoord),
-        },
-        });
-    }
-    
-    for (const edge of edges) {
-        await prisma.edges.create({
-        data: {
-            ...edge,
-        },
-        });
-    }
+  for (const node of nodes) {
+    await prisma.nodes.create({
+      data: {
+        ...node,
+        xcoord: Number(node.xcoord),
+        ycoord: Number(node.ycoord),
+      },
+    });
+  }
+
+  for (const edge of edges) {
+    await prisma.edges.create({
+      data: {
+        ...edge,
+      },
+    });
+  }
 };
 
 try {
-    await main();
-    await prisma.$disconnect();
+  await main();
+  await prisma.$disconnect();
 } catch (e) {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
+  console.error(e);
+  await prisma.$disconnect();
+  process.exit(1);
 }
