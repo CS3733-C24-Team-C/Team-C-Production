733f5bede6478c3678181b8c034e0903de1a7bcd
jlamenha <jlamenhamiranda@wpi.edu>
2024-01-29 19:05:48 -0500

    migrated database frame with requests
    updated Nodes model in prisma schema.prisma to include a list of Requests in a single node
    
    updated seed.mjs to include statements to read a Requests.csv file (currently commented out)

--- packages/database/prisma/seed.mjs
+++ packages/database/prisma/seed.mjs
@@ -1,39 +1,53 @@
 import path from "path";
-import { PrismaClient } from '../.prisma/client';
+import { PrismaClient, Prisma } from "../.prisma/client";
 import { readCSV } from "./csvReader.mjs";
 
 const prisma = new PrismaClient();
 const nodesPath = path.join(path.resolve(), "prisma/L1Nodes.csv");
 const edgesPath = path.join(path.resolve(), "prisma/L1Edges.csv");
+//const requestsPath = path.join(path.resolve(), "prisma/Requests.csv");
 
 const main = async () => {
-    const nodes = readCSV(nodesPath);
-    const edges = readCSV(edgesPath);
+  const nodes = readCSV(nodesPath);
+  const edges = readCSV(edgesPath);
+  //const requests = readCSV(requestsPath);
 
-    for (const node of nodes) {
-        await prisma.nodes.create({
-        data: {
-            ...node,
-            xcoord: Number(node.xcoord),
-            ycoord: Number(node.ycoord),
-        },
-        });
-    }
-    
-    for (const edge of edges) {
-        await prisma.edges.create({
-        data: {
-            ...edge,
-        },
-        });
-    }
+  for (const node of nodes) {
+    await prisma.nodes.create({
+      data: {
+        ...node,
+        xcoord: Number(node.xcoord),
+        ycoord: Number(node.ycoord),
+        //requestData: Prisma.NodesCreateWithoutRequestsInput,
+      },
+    });
+  }
+
+  for (const edge of edges) {
+    await prisma.edges.create({
+      data: {
+        ...edge,
+      },
+    });
+  }
+
+  // for (const request of requests) {
+  //     let i = 0;
+  //     await prisma.requests.create({
+  //         data: {
+  //             id: i,
+  //             ...request,
+  //         },
+  //     });
+  //     i++;
+  // }
 };
 
 try {
-    await main();
-    await prisma.$disconnect();
+  await main();
+  await prisma.$disconnect();
 } catch (e) {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
+  console.error(e);
+  await prisma.$disconnect();
+  process.exit(1);
 }
