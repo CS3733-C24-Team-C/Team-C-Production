44c02b9ac7283fe3de91f14d9b1fd5791f224d6c
jlamenha <jlamenhamiranda@wpi.edu>
2024-01-31 00:57:51 -0500

    handles file upload and saves file to the selected folder
    still needs to be tested with frontend to see if it works as intended
    
    @ts-ignore was used to make the code stop screaming at me, and to test the code before dealing with bugs

--- apps/backend/src/routes/map.ts
+++ apps/backend/src/routes/map.ts
@@ -1,9 +1,10 @@
 import express, { Router, Request, Response } from "express";
 import PrismaClient from "../bin/database-connection.ts";
-//import multer from "multer";
+import multer from "multer";
+import fileUpload from "express-fileupload";
 
 const router: Router = express.Router();
-//const upload = multer({ dest: "../../backend/uploaded-csvs" });
+const upload = multer({ dest: "../../backend/uploaded-csvs" });
 
 router.get("/nodes", async function (req: Request, res: Response) {
   const nodes = await PrismaClient.nodes.findMany();
@@ -15,12 +16,28 @@ router.get("/edges", async function (req: Request, res: Response) {
   res.json(edges);
 });
 
-// router.post(
-//   "/upload",
-//   upload.single("nodeData"),
-//   function (req: Request, res: Response) {
-//     const csvNodes = req.file;
-//   },
-// );
+router.use(fileUpload());
+
+// router.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+router.post(
+  "/upload",
+  upload.single("nodeData"),
+  function (req: Request, res: Response) {
+    if (!req.file || Object.keys(req.file).length === 0) {
+      return res.status(400).send("No file was selected");
+    }
+
+    // Access the uploaded file using the 'sampleFile' field name
+    // @ts-expect-error testing the file upload before dealing with bugs
+    const uploadedFile = req.files.sampleFile;
+
+    uploadedFile.mv("../../backend/uploaded-csvs");
+
+    res.send('File uploaded and saved to: "../../backend/uploaded-csvs"');
+  },
+);
 
 export default router;
