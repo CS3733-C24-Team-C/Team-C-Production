563f3d8f992ac635c91d74067cf92b2966632243
jlamenha <jlamenhamiranda@wpi.edu>
2024-01-30 23:34:51 -0500

    made changes on routing for uploading CSV files from the map page
    changes to map.ts are commented out
    
    added the multer package to package.json

--- apps/backend/src/routes/map.ts
+++ apps/backend/src/routes/map.ts
@@ -1,7 +1,9 @@
 import express, { Router, Request, Response } from "express";
 import PrismaClient from "../bin/database-connection.ts";
+//import multer from "multer";
 
 const router: Router = express.Router();
+//const upload = multer({ dest: "../../backend/uploaded-csvs" });
 
 router.get("/nodes", async function (req: Request, res: Response) {
   const nodes = await PrismaClient.nodes.findMany();
@@ -12,5 +14,13 @@ router.get("/edges", async function (req: Request, res: Response) {
   const edges = await PrismaClient.edges.findMany();
   res.json(edges);
 });
+
+// router.post(
+//   "/upload",
+//   upload.single("nodeData"),
+//   function (req: Request, res: Response) {
+//     const csvNodes = req.file;
+//   },
+// );
 
 export default router;
