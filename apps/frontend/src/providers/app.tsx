8860ca46a5282b7558c2ca6b2922adb72aa634dc
felixng <ngthinh302@gmail.com>
2024-01-31 20:13:20 -0500

    feat(frontend): set up theme provider

--- apps/frontend/src/providers/app.tsx
+++ apps/frontend/src/providers/app.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
+import ThemeProvider from "./theme";
 
 export const AppProvider = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 };
