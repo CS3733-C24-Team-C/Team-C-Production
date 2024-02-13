6a41af8b574d3ad0ca14c5b8e600f5d9934fc925
ptheney <ptheney@wpi.edu>
2024-02-13 14:44:22 -0500

    new wrapper placement for auth0 provider and token access functionality

--- apps/frontend/src/features/auth/routes/SignIn.tsx
+++ apps/frontend/src/features/auth/routes/SignIn.tsx
@@ -1,19 +1,13 @@
-import React /*, { useEffect }*/ from "react";
+import React, { useEffect } from "react";
 import { LoginButton } from "@/features/auth/components/LoginButton.tsx";
 import { LogoutButton } from "@/features/auth/components/LogoutButton.tsx";
 import { SignupButton } from "@/features/auth/components/SignupButton.tsx";
-import { Auth0Provider /*useAuth0*/ } from "@auth0/auth0-react";
-import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const SignIn = () => {
-  const navigate = useNavigate();
-
-  /*
-  const {
-    isAuthenticated,
-    isLoading,
-    user,
-  } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
 
   useEffect(() => {
     const checkToken = async () => {
@@ -31,29 +25,10 @@ const SignIn = () => {
     if (!isLoading && isAuthenticated) {
       checkToken();
     }
-  }, [
-    getAccessTokenSilently,
-    isAuthenticated,
-    isLoading,
-    location.pathname,
-    loginWithRedirect,
-  ]);
-
-   */
+  }, [getAccessTokenSilently, isAuthenticated, isLoading, loginWithRedirect]);
 
   return (
-    <Auth0Provider
-      useRefreshTokens
-      cacheLocation="localstorage"
-      domain="dev-njtak837ng1u41nc.us.auth0.com"
-      clientId="C2UCnUwHJvf1DIbyZHjMGqNyyo56oKS5"
-      onRedirectCallback={(appState) => {
-        navigate(appState?.returnTo || window.location.pathname);
-      }}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
+    <div>
       <h1>Sign In Page:</h1>
       <br></br>
       <LoginButton />
@@ -61,7 +36,7 @@ const SignIn = () => {
       <LogoutButton />
       <br></br>
       <SignupButton />
-    </Auth0Provider>
+    </div>
   );
 };
 
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom/client";
 import "flowbite";
 import "./index.css";
 import App from "./App.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      useRefreshTokens
+      cacheLocation="localstorage"
+      domain="dev-njtak837ng1u41nc.us.auth0.com"
+      clientId="C2UCnUwHJvf1DIbyZHjMGqNyyo56oKS5"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 );
