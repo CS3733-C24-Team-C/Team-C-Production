e8508b017f0f9741fc7e232038b0929b7489c43d
ptheney <ptheney@wpi.edu>
2024-02-19 18:58:35 -0500

    cleaning up unnecessary files before creating login profile

--- apps/frontend/src/features/auth/routes/index.tsx
+++ apps/frontend/src/features/auth/routes/index.tsx
@@ -1,7 +1,5 @@
 import { Outlet, Route, Routes } from "react-router-dom";
-import { SignUp } from "./SignUp";
 import { SignIn } from "./SignIn";
-import { ResetPassword } from "./ResetPassword";
 import { ContentLayout } from "@/components";
 
 const AuthLayout = () => {
@@ -16,9 +14,7 @@ export const AuthRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthLayout />}>
-        <Route path="sign-up" element={<SignUp />} />
         <Route path="sign-in" element={<SignIn />} />
-        <Route path="reset-password" element={<ResetPassword />} />
       </Route>
     </Routes>
   );
