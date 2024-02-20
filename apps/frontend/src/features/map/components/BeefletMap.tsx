9198fe861d097efc1448c8157f40a6b5a9b3b138
Grubbiestea <97642570+Grubbiestea@users.noreply.github.com>
2024-02-20 17:54:21 -0500

    Added animated path and zoom auto reset.

--- apps/frontend/src/features/map/components/BeefletMap.tsx
+++ apps/frontend/src/features/map/components/BeefletMap.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState, useMemo } from "react";
 import {
   MapContainer,
   ImageOverlay,
@@ -10,29 +10,19 @@ import {
   LayerGroup,
   Tooltip,
   SVGOverlay,
+  useMapEvent,
+  useMap,
 } from "react-leaflet";
-import L, { LatLngBounds, CRS } from "leaflet";
+import { LatLngBounds, CRS, LatLng } from "leaflet";
 import { MapContext } from "../components";
 import "./forBeef.css";
 import "leaflet/dist/leaflet.css";
 import { Button } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
 import CustomButton from "@/features/map/components/Description.tsx";
-import { assetToFloor, floorToAsset } from "../utils";
+import { assetToFloor } from "../utils";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Nodes } from "database";
-import ElevatorIcon from "../assets/ElevatorIcon.png";
-import lowerLevel1 from "../assets/00_thelowerlevel1.png";
-import lowerLevel2 from "../assets/00_thelowerlevel2.png";
-import firstFloor from "../assets/01_thefirstfloor.png";
-import secondFloor from "../assets/02_thesecondfloor.png";
-import thirdFloor from "../assets/03_thethirdfloor.png";
-
-lowerLevel1;
-lowerLevel2;
-firstFloor;
-secondFloor;
-thirdFloor;
 
 export default function BeefletMap() {
   // Define the bounds of the image in terms of x and y coordinates
@@ -42,61 +32,43 @@ export default function BeefletMap() {
     edges,
     algorithm,
     selectedFloor,
-    setSelectedFloor,
     path,
     setPath,
-    //startLocation,
+    startLocation,
     setStartLocation,
-    //endLocation,
+    endLocation,
     setEndLocation,
   } = useContext(MapContext);
 
   const [toggledEdges, setToggledEdges] = useState(false);
   const [toggledNames, setToggledNames] = useState(false);
-  const [toggledHallways, setToggledHallways] = useState(false);
+  const [colorBlind, setColorBlind] = useState(false);
   const [clicked, setClicked] = useState(false);
-  const [startID, setStartID] = useState("");
-  const [endID, setEndID] = useState("");
+  const [zoom, setZoom] = useState(0);
   const navigate = useNavigate();
   const { isAuthenticated } = useAuth0();
-
+  
   const nodePath = path.map((nodeID) =>
-    nodes.filter((node) => node.nodeID == nodeID),
+    nodes.filter((node) => node.nodeID == nodeID)
   );
 
-  class newNodeFloorID {
-    node: Nodes;
-    floorID: string;
-
-    constructor(node: Nodes, floorID: string) {
-      this.node = node;
-      this.floorID = floorID;
-    }
-  }
-
   let paths = { G: [], L1: [], L2: [], "1": [], "2": [], "3": [] };
-
-  const floorChanges: newNodeFloorID[] = [];
-  const prevFloors: newNodeFloorID[] = [];
 
   if (nodePath.length > 0) {
     paths = { G: [], L1: [], L2: [], "1": [], "2": [], "3": [] };
     let currentFloor = nodePath[0][0].floor;
     let lastCut = 0;
     for (let i = 1; i < nodePath.length; i++) {
-      if (nodePath[i][0].floor != currentFloor) {
-        // @ts-expect-error type error (any)
-        paths[currentFloor].push(nodePath.slice(lastCut, i));
-        currentFloor = nodePath[i][0].floor;
-        lastCut = i;
-        floorChanges.push(new newNodeFloorID(nodePath[i - 1][0], currentFloor));
-        prevFloors.push(
-          new newNodeFloorID(nodePath[i][0], nodePath[i - 1][0].floor),
-        );
-      }
+        if (nodePath[i][0].floor != currentFloor) {
+            // @ts-expect-error type error (any)
+            paths[currentFloor].push(nodePath.slice(lastCut, i));
+            currentFloor = nodePath[i][0].floor;
+            lastCut = i;
+        }
     }
     // @ts-expect-error type error (any)
     paths[currentFloor].push(nodePath.slice(lastCut, nodePath.length));
+    //console.log(paths);
   }
 
   const handleSubmit = async (s: string, e: string) => {
@@ -104,10 +76,10 @@ export default function BeefletMap() {
       return;
     }
     const startNodeId = nodes
-      .filter((node) => node["nodeID"] === s)
+      .filter((node) => node["longName"] === s)
       .map((node) => node.nodeID)[0];
     const endNodeId = nodes
-      .filter((node) => node["nodeID"] === e)
+      .filter((node) => node["longName"] === e)
       .map((node) => node.nodeID)[0];
     try {
       const res = await fetch("/api/map/pathfinding", {
@@ -130,6 +102,71 @@ export default function BeefletMap() {
     }
   };
 
+    function pathToPoints(pathT: Nodes[][]): {
+        pathData: string;
+        pathLength: number;
+    } {
+        //console.log(pathT);
+        let pathData =
+            "M" +
+            pathT[0][0].xcoord +
+            "," +
+            pathT[0][0].ycoord +
+            " ";
+        pathT.slice(1, pathT.length).forEach((node) => {
+            pathData +=
+                "L" +
+                (node[0].xcoord +
+                    "," +
+                    node[0].ycoord +
+                    " ");
+        });
+
+        const pathElement = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "path"
+        );
+        pathElement.setAttribute("d", pathData);
+
+        const pathLength = pathElement.getTotalLength();
+
+        //console.log(pathLength);
+        return { pathData, pathLength };
+    };
+
+    const ZoomGetter = () => {
+      const map = useMapEvent('zoom', (event) => {
+        const zoomLevel = event.target.getZoom();
+        // Do something with the zoomLevel
+        //console.log('Current zoom level:', zoomLevel);
+        setZoom(zoomLevel);
+      });
+      map;
+      // Your component code...
+    
+      return null; // or your JSX
+    };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [map, setMap] = useState<any>(); const [lastFloor, setLastFloor] = useState<any>();
+  const ResetZoom = (map: { flyTo: (arg0: LatLng, arg1: number) => void; }) => {
+    if (lastFloor != selectedFloor) {
+      map.flyTo(new LatLng(-1700,2500), -2);
+      new LatLng(0,0);
+      map;
+    }
+    console.log("test");
+  };
+  //eslint-disable-next-line react-hooks/exhaustive-deps
+  const floor = useMemo(() => {if (map != null) {ResetZoom(map);} return console.log("ran");}, [selectedFloor]);
+  floor;
+
+  function MapGetter() {
+    setMap(useMap());
+    setLastFloor(selectedFloor);
+    return null;
+  }
+
   return (
     <div className="w-full h-full">
       <MapContainer
@@ -142,10 +179,13 @@ export default function BeefletMap() {
         maxBoundsViscosity={1.0}
         bounceAtZoomLimits={true}
         doubleClickZoom={false}
+        
       >
+        <ZoomGetter></ZoomGetter>
+        <MapGetter></MapGetter>
         <LayerGroup>
-          <ImageOverlay url={selectedFloor} bounds={imageBounds} />
-          {toggledEdges &&
+        <ImageOverlay url={selectedFloor} bounds={imageBounds}/>
+          {toggledEdges && 
             edges
               .map((edge) => [
                 nodes.filter((node) => node.nodeID == edge.startNode),
@@ -154,7 +194,7 @@ export default function BeefletMap() {
               .filter(
                 (edge) =>
                   edge[0][0].floor == assetToFloor(selectedFloor) &&
-                  edge[0][0].floor == edge[1][0].floor,
+                  edge[0][0].floor == edge[1][0].floor
               )
               .map((edge) => (
                 <Polyline
@@ -162,53 +202,85 @@ export default function BeefletMap() {
                     [edge[0][0].ycoord * -1, edge[0][0].xcoord],
                     [edge[1][0].ycoord * -1, edge[1][0].xcoord],
                   ]}
+                  pathOptions={{
+                    color: "black"
+                  }}
                 ></Polyline>
               ))}
           {paths[assetToFloor(selectedFloor)].map((currentPath, i) => (
-            <Polyline
-              key={i}
-              //@ts-expect-error any type error
-              positions={currentPath.map((node) => [
-                -node[0].ycoord,
-                node[0].xcoord,
-              ])}
-              pathOptions={{ color: "black" }}
-              interactive={false}
-              weight={8}
-            />
+              <>
+                <SVGOverlay bounds={new LatLngBounds([0, 0], [-3400, 5000])}>
+                    <svg viewBox="0 0 5000 3400" key={i}>
+                        <path
+                            key = {i}
+                            id={"movePath" + i.toString()}
+                            d={pathToPoints(currentPath).pathData}
+                            stroke={(() => {if (colorBlind) {return "purple";} else {return "#0d6102";}})()}
+                            fill="none"
+                            strokeWidth={12 * Math.max(1-zoom, 1)}
+                            strokeLinecap={"round"}
+                        />
+                        <path
+                            key = {i}
+                            id={"movePath" + i.toString()}
+                            d={pathToPoints(currentPath).pathData}
+                            stroke={(() => {if (colorBlind) {return "#5D3A9B";} else {return "green";}})()}
+                            fill="none"
+                            strokeWidth={6 * Math.max(1-zoom, 1)}
+                            strokeLinecap={"round"}
+                        />
+                        {(() => {
+                            const path = pathToPoints(currentPath);
+                            const pathLength = path.pathLength;
+                            const pathData = path.pathData;
+                            const numDots = Math.floor(pathLength / 10);
+                            return [...Array(numDots)].map((_, index) => (
+                                <>
+                                    <circle key={index} r={3 * Math.max(1-zoom, 1)}
+                                    fill={(() => {if (colorBlind) {return "#E66100";} else {return "yellow";}})()}>
+                                        <animateMotion
+                                            dur={Math.floor(numDots / 4).toString() + "s"}
+                                            repeatCount="indefinite"
+                                            begin={index}
+                                            path={pathData}
+                                        ></animateMotion>
+                                    </circle>
+                                </>
+                            ));
+                        })()}
+                    </svg>
+                </SVGOverlay>
+              </>
           ))}
         </LayerGroup>
-        <SVGOverlay bounds={imageBounds}></SVGOverlay>
-        <FeatureGroup>
-          {nodes
-            .filter((node) => node.floor == assetToFloor(selectedFloor))
-            .filter((node) => {
-              if (toggledHallways) {
-                return true;
-              }
-              return node.nodeType != "HALL";
-            })
-            .map((node, i) => {
-              return (
-                <Circle
-                  key={i}
-                  center={[-node.ycoord, node.xcoord]}
+          <FeatureGroup>
+              {nodes
+                  .filter((node) => node.floor == assetToFloor(selectedFloor))
+                  .filter((node) => node.nodeType != "HALL")
+                  .map((node, i) => {
+                      return (
+                          <Circle
+                              key={i}
+                              center={[-node.ycoord, node.xcoord]}
+                              fillOpacity={1}
                   radius={(() => {
-                    if (node.nodeID == startID) {
+                    if (node.longName == startLocation) {
                       return 10;
-                    } else if (node.nodeID == endID) {
+                    } else if (node.longName == endLocation) {
                       return 10;
                     }
                     return 7;
                   })()}
                   pathOptions={{
-                    color: (() => {
-                      if (node.nodeID == startID) {
+                    color: "#0E7490",
+                    weight: 2,
+                    fillColor: (() => {
+                      if (node.longName == startLocation) {
                         return "blue";
-                      } else if (node.nodeID == endID) {
-                        return "red";
+                      } else if (node.longName == endLocation) {
+                        return "yellow";
                       }
-                      return "green";
+                      return "#52BAC2";
                     })(),
                   }}
                   eventHandlers={{
@@ -226,8 +298,7 @@ export default function BeefletMap() {
                       if (clicked || e.originalEvent.ctrlKey) {
                         e.target.closePopup();
                         setStartLocation(node.longName);
-                        setStartID(node.nodeID);
-                        await handleSubmit(node.nodeID, endID);
+                        await handleSubmit(node.longName, endLocation);
                         return;
                       }
                       setClicked(true);
@@ -236,11 +307,9 @@ export default function BeefletMap() {
                       //e.target.preventDefault();
                       e.target.closePopup();
                       setEndLocation(node.longName);
-                      setEndID(node.nodeID);
-                      await handleSubmit(startID, node.nodeID);
+                      await handleSubmit(startLocation, node.longName);
                     },
                   }}
-                  fillOpacity={0.8}
                 >
                   <Popup className="leaflet-popup-content-wrapper">
                     {clicked ? (
@@ -248,8 +317,7 @@ export default function BeefletMap() {
                         <Button
                           onClick={async () => {
                             setStartLocation(node.longName);
-                            setStartID(node.nodeID);
-                            await handleSubmit(node.nodeID, endID);
+                            await handleSubmit(node.longName, endLocation);
                           }}
                           className={"custom-button"}
                         >
@@ -258,8 +326,7 @@ export default function BeefletMap() {
                         <Button
                           onClick={async () => {
                             setEndLocation(node.longName);
-                            setEndID(node.nodeID);
-                            await handleSubmit(startID, node.nodeID);
+                            await handleSubmit(startLocation, node.longName);
                           }}
                           className={"custom-button"}
                         >
@@ -294,6 +361,8 @@ export default function BeefletMap() {
                         {"Short name: " + node.shortName}
                         <br />
                         {"Node ID: " + node.nodeID}
+                        <br />
+                        {"Node type: " + node.nodeType}
                       </div>
                     )}
                   </Popup>
@@ -313,8 +382,8 @@ export default function BeefletMap() {
         {nodes
           .filter(
             (node) =>
-              node.nodeID == startID &&
-              node.floor == assetToFloor(selectedFloor),
+              node.longName == startLocation &&
+              node.floor == assetToFloor(selectedFloor)
           )
           .map((node) => (
             <Marker position={[-node.ycoord, node.xcoord]} key={node.nodeID} />
@@ -322,70 +391,11 @@ export default function BeefletMap() {
         {nodes
           .filter(
             (node) =>
-              node.nodeID == endID && node.floor == assetToFloor(selectedFloor),
+              node.longName == endLocation &&
+              node.floor == assetToFloor(selectedFloor)
           )
           .map((node) => (
             <Marker position={[-node.ycoord, node.xcoord]} key={node.nodeID} />
-          ))}
-        {floorChanges
-          .filter(
-            (newFloor) => newFloor.node.floor == assetToFloor(selectedFloor),
-          )
-          .map((newFloor) => (
-            <Marker
-              position={[-newFloor.node.ycoord, newFloor.node.xcoord]}
-              key={newFloor.node.nodeID}
-              icon={L.icon({
-                iconUrl: ElevatorIcon,
-                iconSize: [20, 20], // Adjust size if needed
-                iconAnchor: [10, 10],
-                tooltipAnchor: [0, -10],
-              })}
-              eventHandlers={{
-                click: async () =>
-                  setSelectedFloor(
-                    adhocConverterChangePlease(newFloor.floorID),
-                  ),
-              }}
-            >
-              <Tooltip
-                permanent={true}
-                className={"customTooltip"}
-                direction={"top"}
-              >
-                {"Proceed to Floor " + newFloor.floorID}
-              </Tooltip>
-            </Marker>
-          ))}
-        {prevFloors
-          .filter(
-            (newFloor) => newFloor.node.floor == assetToFloor(selectedFloor),
-          )
-          .map((newFloor) => (
-            <Marker
-              position={[-newFloor.node.ycoord, newFloor.node.xcoord]}
-              key={newFloor.node.nodeID}
-              icon={L.icon({
-                iconUrl: ElevatorIcon,
-                iconSize: [20, 20], // Adjust size if needed
-                iconAnchor: [10, 10],
-                tooltipAnchor: [0, -10],
-              })}
-              eventHandlers={{
-                click: async () =>
-                  setSelectedFloor(
-                    adhocConverterChangePlease(newFloor.floorID),
-                  ),
-              }}
-            >
-              <Tooltip
-                permanent={true}
-                className={"customTooltip"}
-                direction={"top"}
-              >
-                {"Arrive from Floor " + newFloor.floorID}
-              </Tooltip>
-            </Marker>
           ))}
         <div>
           <CustomButton
@@ -401,8 +411,8 @@ export default function BeefletMap() {
             position={"bottomleft"}
           />
           <CustomButton
-            title={"Toggle Hallways"}
-            onClick={() => setToggledHallways(!toggledHallways)}
+            title={"Toggle Colorblind"}
+            onClick={() => setColorBlind(!colorBlind)}
             className={"custom-toggle-button"}
             position={"bottomleft"}
           />
@@ -412,8 +422,18 @@ export default function BeefletMap() {
   );
 }
 
-const adhocConverterChangePlease = (floorID: string) => {
-  //const floor = floorID.substring(0, floorID.length - 1);
-  // @ts-expect-error nope
-  return floorToAsset(floorID);
-};
+/*
+dur={Math.floor(numDots / 3).toString() + "s"}
+
+<Polyline
+              key={i}
+              //@ts-expect-error any type error
+              positions={currentPath.map((node) => [
+                -node[0].ycoord,
+                node[0].xcoord,
+              ])}
+              pathOptions={{ color: "black" }}
+              interactive={false}
+              weight={8}
+            />
+ */
