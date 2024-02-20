c6a5a9632da7d9aee339ec532adfc683886816d4
jlamenha <jlamenhamiranda@wpi.edu>
2024-02-20 17:51:47 -0500

    updated prisma schema

--- packages/database/prisma/migrations/20240220225001_employee_delete/migration.sql
+++ packages/database/prisma/migrations/20240220225001_employee_delete/migration.sql
@@ -5,4 +5,4 @@ ALTER TABLE "Requests" DROP CONSTRAINT "Requests_employeeID_fkey";
 ALTER TABLE "Requests" ALTER COLUMN "employeeID" DROP NOT NULL;
 
 -- AddForeignKey
-ALTER TABLE "Requests" ADD CONSTRAINT "Requests_employeeID_fkey" FOREIGN KEY ("employeeID") REFERENCES "Employees"("id") ON DELETE SET NULL ON UPDATE SET NULL;
+ALTER TABLE "Requests" ADD CONSTRAINT "Requests_employeeID_fkey" FOREIGN KEY ("employeeID") REFERENCES "Employees"("id") ON DELETE SET NULL ON UPDATE CASCADE;
