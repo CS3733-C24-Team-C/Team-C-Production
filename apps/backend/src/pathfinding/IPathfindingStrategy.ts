ae74d50eb9c397fbf8b7fad0c122d8fe36fa9903
Reda Boutayeb <87339534+RedaB2@users.noreply.github.com>
2024-02-20 20:57:17 -0500

    Interface for Strategy

---
+++ apps/backend/src/pathfinding/IPathfindingStrategy.ts
@@ -0,0 +1,7 @@
+import { Graph } from "./GraphSingleton.ts";
+
+interface IPathfindingStrategy {
+    findPath(startNodeId: string, endNodeId: string, graph: Graph): Promise<string[]>;
+}
+
+export type {IPathfindingStrategy};
